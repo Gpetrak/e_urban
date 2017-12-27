2f1e62cb79a9453098bd52dd13c242863196361b
gpetrak <gkpetrak@gmail.com>
2017-12-27 17:50:35 +0200

    opening pdf files

--- urls.py
+++ urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, url
-from crete_gis.e_urban.views import urban_home, urban_map, create_get
+from crete_gis.e_urban.views import urban_home, urban_map, create_get, pdf_home
 
 
 urlpatterns = patterns(
     'crete_gis.e_urban',
     url(r'^$', urban_home, name='urban_home'),
     url(r'^e_urban_map/$', urban_map, name='urban_map'),
-    url(r'^ajax/results/', create_get, name='create_get')
+    url(r'^ajax/results/', create_get, name='create_get'),
+    url(r'^static/pdf/(?P<filename>)/$', pdf_home, name='pdf_home')
     )
 
 
